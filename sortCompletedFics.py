cb7cd688f73ce58ef2b11c92bb2ce91d2df89f59
Beck Sewart <97924634+bairdel@users.noreply.github.com>
2026-04-10 21:24:22 +0100

    trying to remove need for "posted Chapter" filter in yahoo and long run of mcyt

--- sortCompletedFics.py
+++ sortCompletedFics.py
@@ -9,7 +9,7 @@ import regex
 from time import sleep
 
 debug = False
-numMessages = 50
+numMessages = 3000
 
 
 def pickFolder(folders, message):
@@ -21,21 +21,21 @@ def pickFolder(folders, message):
 
     
 inboxes = [
-    {'base': '"Avatar the Last Airbender"', 'wips': '"Avatar the Last Airbender/ATLA WIPs"', '3k': '"Avatar the Last Airbender/ATLA 3K"', '20k':'"Avatar the Last Airbender/ATLA 20K"'},
-            {'base': '"BNHA/Midoriya Izuku"', 'wips': '"BNHA/Midoriya Izuku WIPs"', '3k': '"BNHA/Midoriya Izuku 3K"', '20k':'"BNHA/BNHA 20K"'},
-            {'base': '"Danny Phantom"', 'wips': '"Danny Phantom"', '3k': '"Danny Phantom/Danny Phantom 3K"', '20k':'"Danny Phantom/Danny Phantom 20K"'},
-            {'base': '"DC/Jason Todd, Tim Drake"', 'wips': '"DC/Jason Todd, Tim Drake"', '3k': '"DC/JTTD 3K"', '20k':'"DC/DC 20K"'},
-            {'base': '"DSMP"', 'wips': '"DSMP/DSMP WIPs"', '3k': '"DSMP/DSMP 3K"', '20k':'"DSMP/DSMP 3K"'},
-            {'base': '"Good Omens"', 'wips': '"Good Omens"', '3k': '"Good Omens/Good Omens 3K"', '20k':'"Good Omens/Good Omens 20K"'},
-            {'base': '"Harry Potter"', 'wips': '"Harry Potter/Harry Potter WIPs"', '3k': '"Harry Potter/HP 3K"', '20k':'"Harry Potter/HP 20K"'},
-            {'base': '"Marvel"', 'wips': '"Marvel/Marvel WIPs"', '3k': '"Marvel/Marvel 3K"', '20k':'"Marvel/Marvel 20K"'},
-            {'base': '"Merlin"', 'wips': '"Merlin/Merlin WIPs"', '3k': '"Merlin"', '20k':'"Merlin"'},
-            {'base': '"Miraculous Ladybug"', 'wips': '"Miraculous Ladybug/Miraculous WIPs"', '3k': '"Miraculous Ladybug/Miraculous 3K"', '20k':'"Miraculous Ladybug/Miraculous 20K"'},
-            {'base': '"Others"', 'wips': '"Others"', '3k': '"Others/Others 3K"', '20k':'"Misc 20K"'},
-            {'base': '"Percy Jackson"', 'wips': '"Percy Jackson/PJO WIPs"', '3k': '"Percy Jackson"', '20k':'"Percy Jackson"'},
-            {'base': '"Star Wars"', 'wips': '"Star Wars/Star Wars WIPs"', '3k': '"Star Wars/Star Wars 3K"', '20k':'"Star Wars/Star Wars 20K"'},
-            {'base': '"The Murderbot Diaries"', 'wips': '"The Murderbot Diaries"', '3k': '"The Murderbot Diaries/The Murderbot Diaries 3K"', '20k':'"The Murderbot Diaries/The Murderbot Diaries 20K"'},
-            {'base': '"The Untamed"', 'wips': '"The Untamed/Untamed WIPs"', '3k': '"The Untamed/The Untamed 3K"', '20k':'"The Untamed/The Untamed 20K"'}
+    # {'base': '"Avatar the Last Airbender"', 'wips': '"Avatar the Last Airbender/ATLA WIPs"', '3k': '"Avatar the Last Airbender/ATLA 3K"', '20k':'"Avatar the Last Airbender/ATLA 20K"'},
+    #         {'base': '"BNHA/Midoriya Izuku"', 'wips': '"BNHA/Midoriya Izuku WIPs"', '3k': '"BNHA/Midoriya Izuku 3K"', '20k':'"BNHA/BNHA 20K"'},
+    #         {'base': '"Danny Phantom"', 'wips': '"Danny Phantom"', '3k': '"Danny Phantom/Danny Phantom 3K"', '20k':'"Danny Phantom/Danny Phantom 20K"'},
+    #         {'base': '"DC/Jason Todd, Tim Drake"', 'wips': '"DC/Jason Todd, Tim Drake"', '3k': '"DC/JTTD 3K"', '20k':'"DC/DC 20K"'},
+            {'base': '"MCYT"', 'wips': '"MCYT/MCYT WIPs"', '3k': '"MCYT/MCYT 3K"', '20k':'"MCYT/MCYT 20K"'},
+            # {'base': '"Good Omens"', 'wips': '"Good Omens"', '3k': '"Good Omens/Good Omens 3K"', '20k':'"Good Omens/Good Omens 20K"'},
+            # {'base': '"Harry Potter"', 'wips': '"Harry Potter/Harry Potter WIPs"', '3k': '"Harry Potter/HP 3K"', '20k':'"Harry Potter/HP 20K"'},
+            # {'base': '"Marvel"', 'wips': '"Marvel/Marvel WIPs"', '3k': '"Marvel/Marvel 3K"', '20k':'"Marvel/Marvel 20K"'},
+            # {'base': '"Merlin"', 'wips': '"Merlin/Merlin WIPs"', '3k': '"Merlin"', '20k':'"Merlin"'},
+            # {'base': '"Miraculous Ladybug"', 'wips': '"Miraculous Ladybug/Miraculous WIPs"', '3k': '"Miraculous Ladybug/Miraculous 3K"', '20k':'"Miraculous Ladybug/Miraculous 20K"'},
+            # {'base': '"Others"', 'wips': '"Others"', '3k': '"Others/Others 3K"', '20k':'"Misc 20K"'},
+            # {'base': '"Percy Jackson"', 'wips': '"Percy Jackson/PJO WIPs"', '3k': '"Percy Jackson"', '20k':'"Percy Jackson"'},
+            # {'base': '"Star Wars"', 'wips': '"Star Wars/Star Wars WIPs"', '3k': '"Star Wars/Star Wars 3K"', '20k':'"Star Wars/Star Wars 20K"'},
+            # {'base': '"The Murderbot Diaries"', 'wips': '"The Murderbot Diaries"', '3k': '"The Murderbot Diaries/The Murderbot Diaries 3K"', '20k':'"The Murderbot Diaries/The Murderbot Diaries 20K"'},
+            # {'base': '"The Untamed"', 'wips': '"The Untamed/Untamed WIPs"', '3k': '"The Untamed/The Untamed 3K"', '20k':'"The Untamed/The Untamed 20K"'}
             ]
 
 
@@ -100,6 +100,8 @@ def sortEmail(imap, inbox, folders, numMessages):
                                 if content_type == "text/plain" and "attachment" not in content_disposition:
                                     # print text/plain emails and skip attachments
                                     try:
+                                        if "posted Chapter " in subject:
+                                            imap.uid('MOVE', uid ,folders['wips'])
                                         
                                         # print(body.split("Chapters: ")[1].split("\nFandom:")[0])
                                         # chapters = (body.split("Chapters: ")[1].split("\nFandom:")[0]).strip(" ").strip("\r").split("/")
